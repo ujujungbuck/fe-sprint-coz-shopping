ac802638b0b5ed8e660044eefe4751d5119faba5
ujujungbuck <garam8u2@gmail.com>
2023-05-26 10:57:54 +0900

    Toast구현해보기

---
+++ coz-shopping/src/stories/Toast.stories.js
@@ -0,0 +1,13 @@
+import { Toast } from "./Toast";
+
+export default {
+  title: "Example/Toast",
+  component: Toast,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    label: "⭐️ 북마크에 추가되었습니다.",
+  },
+};
